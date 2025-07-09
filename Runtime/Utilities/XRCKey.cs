68e56784108139a9accf49d8a7883cfba2cbc358
Harald Haraldsson <530725+haraldsson@users.noreply.github.com>
2025-07-09 14:09:33 -0400

    Add `XRCKey` ScriptableObject

---
+++ Runtime/Utilities/XRCKey.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace XRC.Toolkit.Core
+{
+    [CreateAssetMenu(fileName = "XRCKey", menuName = "Scriptable Objects/XRC/XRC Key")]
+    public class XRCKey : ScriptableObject
+    {
+        public string key;
+    }
+}
